51e5fce0588a7ba9e607a0f6ee1cfdeb3a180b4f
irvanalfaridzi@gmail.com <irvanalfaridzi@gmail.com>
2021-05-29 10:19:35 +0700

    finshing

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -311,22 +311,198 @@ class MainPage extends StatelessWidget {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
+                        CardRecommendation(
+                          judulRecommendation: "High Fly",
+                          cityLocation: "Gambir, Jakarta",
+                          harga: 430,
+                          rating: 4.6,
+                          urlImage: "images/recomen-1.png",
+                        ),
+                        CardRecommendation(
+                          judulRecommendation: "Stone Mountain",
+                          cityLocation: "Pandalarang, Bandung",
+                          harga: 700,
+                          rating: 4.9,
+                          urlImage: "images/recomen-2.png",
+                        ),
+                        CardRecommendation(
+                          judulRecommendation: "White Building",
+                          cityLocation: "Kebun Raya, Bogor",
+                          harga: 380,
+                          rating: 4.8,
+                          urlImage: "images/recomen-3.png",
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Text(
+                      'Gallery',
+                      style: GoogleFonts.poppins(
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'This photo is from some of EzStay\nFavorite buildings',
+                      style: GoogleFonts.poppins(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.grey,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
                         Container(
-                          width: (MediaQuery.of(context).size.width - 296) / 3,
-                          height: 322,
-                          padding: EdgeInsets.all(20),
+                          width: 400,
+                          height: 480,
                           decoration: BoxDecoration(
-                            color: whiteColor,
-                            borderRadius: BorderRadius.circular(20),
+                            borderRadius: BorderRadius.circular(15),
+                            image: DecorationImage(
+                              image: AssetImage("images/gallery-1.png"),
+                            ),
                           ),
-                          child: Column(
-                            children: [
-                              ClipRRect(
-                                borderRadius: BorderRadius.circular(10),
-                                child: Image.asset("images/recomen-1.png"),
-                              )
-                            ],
-                          ),
+                        ),
+                        Stack(
+                          children: [
+                            Container(
+                              width: 400,
+                              height: 480,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                image: DecorationImage(
+                                  image: AssetImage("images/gallery-2.png"),
+                                ),
+                              ),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  color: Colors.black.withOpacity(0.3),
+                                ),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Container(
+                                      margin: EdgeInsets.only(
+                                        top: 50,
+                                        left: 30,
+                                      ),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            "Glasses House",
+                                            style: GoogleFonts.poppins(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.w600,
+                                              color: whiteColor,
+                                            ),
+                                          ),
+                                          Text(
+                                            "KBP, Bnadung",
+                                            style: GoogleFonts.poppins(
+                                              fontSize: 14,
+                                              fontWeight: FontWeight.w500,
+                                              color: secondaryColor,
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.end,
+                                      children: [
+                                        Container(
+                                          margin: EdgeInsets.only(top: 55),
+                                          width: 60,
+                                          height: 30,
+                                          decoration: BoxDecoration(
+                                            borderRadius: BorderRadius.only(
+                                              topLeft: Radius.circular(15),
+                                              bottomLeft: Radius.circular(15),
+                                            ),
+                                            color: bgNumberColor,
+                                          ),
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceEvenly,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.center,
+                                            children: [
+                                              Image.asset(
+                                                'images/star-1.png',
+                                                width: 20,
+                                                height: 20,
+                                              ),
+                                              Text(
+                                                "4.9",
+                                                style: GoogleFonts.poppins(
+                                                  fontWeight: FontWeight.w600,
+                                                  color: numberColor,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.bottomCenter,
+                              child: Container(
+                                margin: EdgeInsets.fromLTRB(160, 350, 160, 50),
+                                width: 80,
+                                height: 80,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(50),
+                                    color: btnColor),
+                                child: Icon(
+                                  Icons.keyboard_arrow_down_rounded,
+                                  color: whiteColor,
+                                  size: 50,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Container(
+                              width: 340,
+                              height: 215,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                image: DecorationImage(
+                                  image: AssetImage("images/gallery-3.png"),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 50,
+                            ),
+                            Container(
+                              width: 340,
+                              height: 215,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                image: DecorationImage(
+                                  image: AssetImage("images/gallery-4.png"),
+                                ),
+                              ),
+                            )
+                          ],
                         )
                       ],
                     )
@@ -337,6 +513,138 @@ class MainPage extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class CardRecommendation extends StatelessWidget {
+  final String urlImage;
+  final String judulRecommendation;
+  final String cityLocation;
+  final int harga;
+  final double rating;
+  CardRecommendation({
+    this.urlImage,
+    this.judulRecommendation,
+    this.cityLocation,
+    this.harga,
+    this.rating,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          width: (MediaQuery.of(context).size.width - 296) / 3,
+          height: 322,
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: whiteColor,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset(urlImage),
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      margin: EdgeInsets.only(
+                        right: 20,
+                        top: 20,
+                      ),
+                      width: 40,
+                      height: 20,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: bgNumberColor,
+                      ),
+                      child: Center(
+                        child: Text(
+                          '$rating',
+                          style: GoogleFonts.poppins(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w600,
+                            color: numberColor,
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "$judulRecommendation",
+                        style: GoogleFonts.poppins(
+                            fontSize: 20, fontWeight: FontWeight.w500),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Icon(
+                            Icons.location_on_outlined,
+                            color: secondaryColor,
+                          ),
+                          Text(
+                            '$cityLocation',
+                            style: GoogleFonts.poppins(
+                              color: secondaryColor,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        "\$ $harga",
+                        style: GoogleFonts.poppins(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w500,
+                          color: numberColor,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 6,
+                      ),
+                      Text(
+                        "Per Hour",
+                        style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.w500,
+                          color: secondaryColor,
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
